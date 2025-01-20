08d2ed0594883cda643afd1aef963fd0b17aa63c
Kshitiz Mhto <kshitizmhto101@gmail.com>
2025-01-20 20:01:42 +0545

    core: updated core root file for cli for MVP

--- cmd/root.go
+++ cmd/root.go
@@ -22,21 +22,22 @@ import (
 	"github.com/spf13/cobra"
 )
 
-
+var version bool
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "dsync",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "CLI for data synchronization and management",
+	Long: `dSync is a CLI tool designed to simplify the management and synchronization of data 
+between diverse systems, including databases, cloud storage, and local file systems.
+	 `,
+	Run: func(cmd *cobra.Command, args []string) {
+		if version {
+			versionCMD.Run(cmd, args)
+		} else {
+			cmd.Help()
+		}
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -50,14 +51,6 @@ func Execute() {
 
 func init() {
 	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
 
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.dsync.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.Flags().BoolVarP(&version, "version", "v", false, "Print the version of the CLI")
 }
-
-
