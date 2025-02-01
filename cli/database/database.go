0b2b971ed03ac826186b8923e731109170aeba4f
Kshitiz Mhto <kshitizmhto101@gmail.com>
2025-02-02 03:46:29 +0545

    feat: added function call for alert and retrival of failed log to attach with alert

--- cli/database/database.go
+++ cli/database/database.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/Kshitiz-Mhto/xnap/cli/alert"
 	"github.com/Kshitiz-Mhto/xnap/cli/logs"
 	"github.com/Kshitiz-Mhto/xnap/pkg/config"
 	"github.com/Kshitiz-Mhto/xnap/utility"
@@ -116,8 +117,9 @@ func logCommand(dbType, dbUser, dbPassword, host, port, action, command, status,
 }
 
 func AddLogToMysql(dbUser, dbPassword, host, port string, logEntry *logs.Log) error {
-	dns := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPassword, host, port, config.Envs.LOG_DB)
+	var lastLogEntry logs.Log
 
+	dns := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPassword, host, port, config.Envs.LOG_DB)
 	db, err := gorm.Open(mysql.Open(dns), &gorm.Config{})
 	if err != nil {
 		utility.Error("Failed to connect to MySQL: %s", err)
@@ -127,12 +129,34 @@ func AddLogToMysql(dbUser, dbPassword, host, port string, logEntry *logs.Log) er
 	if err := db.Create(logEntry).Error; err != nil {
 		return err
 	}
+
+	if err := db.Order("created_at DESC").First(&lastLogEntry).Error; err != nil {
+		fmt.Println("Error fetching the last row:", err)
+	}
+
+	if logEntry.Status == config.Envs.BACKUP_OR_RESTORE_STATUS {
+		vars := map[string]interface{}{
+			"ID":                lastLogEntry.ID,
+			"Action":            lastLogEntry.Action,
+			"Command":           lastLogEntry.Command,
+			"Status":            lastLogEntry.Status,
+			"ErrorMessage":      lastLogEntry.ErrorMessage,
+			"UserName":          lastLogEntry.UserName,
+			"ExecutionDuration": lastLogEntry.ExecutionDuration,
+			"CreatedAt":         lastLogEntry.CreatedAt,
+			"UpdatedAt":         lastLogEntry.UpdatedAt,
+			"dbType":            dbType,
+		}
+		alert.HTMLTemplateEmailHandler(config.Envs.OWNER_EMAIL, vars)
+	}
 	utility.Success("Log is enteried successfully!!")
 
 	return nil
 }
 
 func AddLogtoPSQL(dbUser, dbPassword, host, port string, logEntry *logs.Log) error {
+	var lastLogEntry logs.Log
+
 	dns := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, dbUser, dbPassword, config.Envs.LOG_DB)
 	db, err := gorm.Open(postgres.Open(dns), &gorm.Config{})
 	if err != nil {
@@ -143,6 +167,28 @@ func AddLogtoPSQL(dbUser, dbPassword, host, port string, logEntry *logs.Log) err
 	if err := db.Create(logEntry).Error; err != nil {
 		return err
 	}
+
+	if err := db.Order("created_at DESC").First(&lastLogEntry).Error; err != nil {
+		return err
+	}
+
+	if logEntry.Status == config.Envs.BACKUP_OR_RESTORE_STATUS {
+		vars := map[string]interface{}{
+			"ID":                lastLogEntry.ID,
+			"Action":            lastLogEntry.Action,
+			"Command":           lastLogEntry.Command,
+			"Status":            lastLogEntry.Status,
+			"ErrorMessage":      lastLogEntry.ErrorMessage,
+			"UserName":          lastLogEntry.UserName,
+			"ExecutionDuration": lastLogEntry.ExecutionDuration,
+			"CreatedAt":         lastLogEntry.CreatedAt,
+			"UpdatedAt":         lastLogEntry.UpdatedAt,
+			"dbType":            dbType,
+		}
+
+		alert.HTMLTemplateEmailHandler(config.Envs.OWNER_EMAIL, vars)
+	}
+
 	utility.Success("Log is enteried successfully!!")
 
 	return nil
